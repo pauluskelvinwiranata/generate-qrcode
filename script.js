451c0c003b6f24953edb37987dab420745fefa80
Paulus <you@example.com>
2023-08-22 08:04:22 +0700

    update

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-let imgBox = document.getElementById("imgBox");
-let qrImage = document.getElementById("qrImage");
-let qrInput = document.getElementById("qrInput");
-
-function generateQR() {
-  if (qrInput.value.length > 0) {
-    qrImage.src =
-      "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
-      qrInput.value;
-    imgBox.classList.add("show-img");
-  }
-}
+let imgBox=document.getElementById("imgBox"),qrImage=document.getElementById("qrImage"),qrInput=document.getElementById("qrInput");function generateQR(){qrInput.value.length>0&&(qrImage.src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+qrInput.value,imgBox.classList.add("show-img"))}
